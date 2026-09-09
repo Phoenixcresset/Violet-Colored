b669e59a45f483723754e1c78dff586e5cf5ad0e
VioletEverbloom <86055389+VioletEverbloom@users.noreply.github.com>
2026-09-09 20:21:25 +0200

    remove used scripts

--- kubejs/server_scripts/tags/removals.js
+++ kubejs/server_scripts/tags/removals.js
@@ -1,8 +1,5 @@
 (() => {
   const removedTags = {
-    block: [
-      "minecraft:enderman_holdable", // Remove enderman griefing
-    ],
     entity_type: [
       "horseman:cannot_swim",
     ],
--- kubejs/server_scripts/tags/removals_from_entries.js
+++ kubejs/server_scripts/tags/removals_from_entries.js
@@ -1,20 +1,8 @@
 (() => {
   const removedTagsFromEntries = {
     item: {
-      "curios:belt": ["supplementaries:quiver", "#supplementaries:keys"],
       // Removes the dragon head to count as an arthropod since Supplementaries adds a spider head
       "dummmmmmy:arthropod_heads": ["minecraft:dragon_head"],
-      "supplementaries:blackboard_black": [
-        "minecraft:charcoal",
-        "minecraft:coal",
-        "#minecraft:coals",
-      ],
-      "supplementaries:blackboard_white": [
-        "minecraft:quartz",
-        "#c:gems/quartz",
-      ],
-      "trinkets:legs/key": ["supplementaries:key"],
-      "trinkets:legs/quiver": ["supplementaries:quiver"],
     },
   };
 
